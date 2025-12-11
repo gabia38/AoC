57bd209ac2c04de5d1ea842e477c7cdd36d32bf9
gabia38 <140528196+gabia38@users.noreply.github.com>
2025-12-11 10:42:40 +0200

    Create Part1.cpp

---
+++ 2025/Day11/Part1.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    unordered_map<string,vector<string>> adj;
+    string source;
+
+    while(cin>>source)
+    {
+        source=source.substr(0,3);
+
+        string dest;
+
+        while(cin.peek()!='\n')
+        {
+            cin>>dest;
+
+            adj[source].push_back(dest);
+        }
+    }
+
+    unordered_map<string,int> freq;   
+    queue<string> bfs;
+    bfs.push("you");
+    freq["you"] = 1;
+
+    while(!bfs.empty())
+    {
+        string s = bfs.front();
+
+        bfs.pop();
+
+        for(auto d : adj[s])
+        {
+            if(freq[d]==0)
+            {
+                bfs.push(d);
+            }
+
+            freq[d]+=freq[s];
+        }
+    }
+
+    cout << freq["out"];
+    return 0;
+}
