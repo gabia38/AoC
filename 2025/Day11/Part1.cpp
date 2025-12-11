73e7436711b0ad4d902f7930b961aa7e877188bd
gabia38 <140528196+gabia38@users.noreply.github.com>
2025-12-11 11:06:07 +0200

    Update Part1.cpp

--- 2025/Day11/Part1.cpp
+++ 2025/Day11/Part1.cpp
@@ -4,42 +4,42 @@ using namespace std;
 
 int main()
 {
-    unordered_map<string,vector<string>> adj;
+    unordered_map<string, vector<string>> adj;
     string source;
 
-    while(cin>>source)
+    while (cin >> source)
     {
-        source=source.substr(0,3);
+        source = source.substr(0, 3);
 
         string dest;
 
-        while(cin.peek()!='\n')
+        while (cin.peek() != '\n')
         {
-            cin>>dest;
+            cin >> dest;
 
             adj[source].push_back(dest);
         }
     }
 
-    unordered_map<string,int> freq;   
+    unordered_map<string, int> freq;
     queue<string> bfs;
     bfs.push("you");
     freq["you"] = 1;
 
-    while(!bfs.empty())
+    while (!bfs.empty())
     {
         string s = bfs.front();
 
         bfs.pop();
 
-        for(auto d : adj[s])
+        for (auto d : adj[s])
         {
-            if(freq[d]==0)
+            if (freq[d] == 0)
             {
                 bfs.push(d);
             }
 
-            freq[d]+=freq[s];
+            freq[d] += freq[s];
         }
     }
 
