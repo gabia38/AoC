297fa8705653ec227ccdf17bd50d7d39b75055de
gabia38 <140528196+gabia38@users.noreply.github.com>
2025-12-19 13:32:34 +0200

    Create Part1.cpp

---
+++ 2023/Day02/Part1.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int rez=0;
+    string line;
+
+    while(getline(cin,line))
+    {
+        unordered_map<string,int> freq;
+        int poz=line.find(':');
+        int id=stoi(line.substr(5,poz-5));
+        
+        string rest=line.substr(poz+1),set;
+        stringstream sets(rest);
+
+        while (getline(sets,set,';'))
+        {
+            string item;
+            stringstream items(set);
+
+            while(getline(items,item,','))
+            {
+                stringstream ss(item);
+                int nr;
+                string color;
+
+                ss>>nr>>color;
+
+                freq[color]=max(freq[color],nr);
+            }
+        }
+        
+        if(freq["red"]<=12&&freq["green"]<=13&&freq["blue"]<=14)
+        {
+            rez+=id;
+        }
+    }
+
+    cout<<rez;
+    return 0;
+}
